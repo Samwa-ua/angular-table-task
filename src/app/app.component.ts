cd0ffddc13f0389a8d43902d5c95289899d07c12
Andrii Skokovskyi <askokovskiy@gmail.com>
2022-09-30 14:45:48 +0300

    feat: Add live rendering with real data from API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,15 @@
 import { Component } from '@angular/core';
 import { TableService } from './table.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface BooksData {
+  id: Number;
+  title: String;
+  description: String;
+  pageCount: Number
+}
 
 @Component({
   selector: 'app-root',
@@ -8,12 +18,15 @@ import { TableService } from './table.service';
 })
 export class AppComponent {
   title = 'angular-table-task';
-  displayedColumns: string[] = ['id', 'bookId', 'title', 'description', 'pageCount'];
+  displayedColumns: string[] = ['id', 'title', 'description', 'pageCount'];
+  dataSource!: MatTableDataSource<BooksData>
+  books: any
 
-  // constructor(private service: TableService) {
-  //   this.service.getData().subscribe((data) => {
-  //     console.log(data);
-
-  //   })
-  // }
+  constructor(private service: TableService) {
+    this.service.getData().subscribe((data) => {
+      console.log(data);
+      this.books = data
+      this.dataSource = new MatTableDataSource(this.books)
+    })
+  }
 }
