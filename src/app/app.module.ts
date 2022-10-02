339f581560354babab7dd5bf8c9fbe0a2914d00f
Andrii Skokovskyi <askokovskiy@gmail.com>
2022-10-02 13:39:51 +0300

    feat: Add styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 import { NgApexchartsModule } from "ng-apexcharts";
 import { ChartComponent } from './chart/chart.component';
 import { TableComponent } from './table/table.component';
@@ -30,7 +31,8 @@ import { TableComponent } from './table/table.component';
     MatSortModule,
     MatFormFieldModule,
     MatInputModule,
-    NgApexchartsModule
+    NgApexchartsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
